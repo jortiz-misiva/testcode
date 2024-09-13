61bb1defb62a3ff69a634f105ea76c78ce3beb86
jortiz-misiva <jrortiz85@hotmail.com>
2024-09-13 17:15:19 -0500

    refactorizacion

--- search_bar.php
+++ search_bar.php
@@ -1,146 +1,58 @@
 <?php
-    if (!defined('MECEXEC')) {
-        wp_die('Acceso directo no permitido.');
-    }
-    $settings = $this->main->get_settings();
-    $modern_type = '';
-    if(isset($settings['search_bar_modern_type']) && $settings['search_bar_modern_type'] == '1') {
-        $modern_type = 'mec-modern-search-bar ';
-    }
-    $output = '<div class="'.esc_attr($modern_type).' mec-wrap mec-search-bar-wrap"><form class="mec-search-form mec-totalcal-box" role="search" method="get" id="searchform" action="'.get_bloginfo('url').'">';
-    if(isset($settings['search_bar_ajax_mode']) && $settings['search_bar_ajax_mode'] == '1'){
-        $output .= '<div class="mec-ajax-search-result">
-            <div class="mec-text-input-search"><i class="mec-sl-magnifier"></i><input type="text" placeholder="'.esc_html__('Please enter at least 3 characters' , 'modern-events-calendar-lite').'" value="" id="keyword" name="keyword" />
-            </div><div id="mec-ajax-search-result-wrap"><div class="mec-ajax-search-result-events">'.esc_html__('Search results will show here' , 'modern-events-calendar-lite').'</div></div>
-        </div>';
-    }else{
-        if(isset($settings['search_bar_text_field']) && $settings['search_bar_text_field'] == '1'){
-            $output .= '<div class="mec-text-input-search"><i class="mec-sl-magnifier"></i><input type="search" value="" id="s" name="s" />
-            </div>';
-        }else{
-            $output .= '<input type="hidden" value="" name="s" />';
-        }
-    }
+if (!defined('MECEXEC')) {
+    wp_die('Acceso directo no permitido.');
+}
+$settings = $this->main->get_settings();
+$modern_type = '';
+if (isset($settings['search_bar_modern_type']) && $settings['search_bar_modern_type'] == '1') {
+    $modern_type = 'mec-modern-search-bar ';
+}
+$output = '<div class="' . esc_attr($modern_type) . ' mec-wrap mec-search-bar-wrap"><form class="mec-search-form mec-totalcal-box" role="search" method="get" id="searchform" action="' . get_bloginfo('url') . '">';
 
-    if((isset($settings['search_bar_category']) && $settings['search_bar_category'] == '1') || (isset($settings['search_bar_location']) && $settings['search_bar_location'] == '1') || (isset($settings['search_bar_organizer']) && $settings['search_bar_organizer'] == '1') || (isset($settings['search_bar_speaker']) && $settings['search_bar_speaker'] == '1') || (isset($settings['search_bar_tag']) && $settings['search_bar_tag'] == '1') || (isset($settings['search_bar_label']) && $settings['search_bar_label'] == '1')){
+if (isset($settings['search_bar_ajax_mode']) && $settings['search_bar_ajax_mode'] == '1') {
+    $output .= '<div class="mec-ajax-search-result">
+        <div class="mec-text-input-search"><i class="mec-sl-magnifier"></i><input type="text" placeholder="' . esc_html__('Please enter at least 3 characters', 'modern-events-calendar-lite') . '" value="" id="keyword" name="keyword" />
+        </div><div id="mec-ajax-search-result-wrap"><div class="mec-ajax-search-result-events">' . esc_html__('Search results will show here', 'modern-events-calendar-lite') . '</div></div>
+    </div>';
+} else {
+    if (isset($settings['search_bar_text_field']) && $settings['search_bar_text_field'] == '1') {
+        $output .= '<div class="mec-text-input-search"><i class="mec-sl-magnifier"></i><input type="search" value="" id="s" name="s" />
+        </div>';
+    } else {
+        $output .= '<input type="hidden" value="" name="s" />';
+    }
+}
+
+$taxonomies = array(
+    'category' => array('key' => 'search_bar_category', 'taxonomy' => 'mec_category', 'icon' => 'folder'),
+    'location' => array('key' => 'search_bar_location', 'taxonomy' => 'mec_location', 'icon' => 'location-pin'),
+    'organizer' => array('key' => 'search_bar_organizer', 'taxonomy' => 'mec_organizer', 'icon' => 'user'),
+    'speaker' => array('key' => 'search_bar_speaker', 'taxonomy' => 'mec_speaker', 'icon' => 'microphone'),
+    'tag' => array('key' => 'search_bar_tag', 'taxonomy' => apply_filters('mec_taxonomy_tag', ''), 'icon' => 'tag'),
+    'label' => array('key' => 'search_bar_label', 'taxonomy' => 'mec_label', 'icon' => 'pin'),
+);
+
+$should_render_dropdown = false;
+foreach ($taxonomies as $tax) {
+    if (isset($settings[$tax['key']]) && $settings[$tax['key']] == '1') {
+        $should_render_dropdown = true;
+        break;
+    }
+}
+
+if ($should_render_dropdown) {
     $output .= '<div class="mec-dropdown-wrap">';
-    
-    if(isset($settings['search_bar_category']) && $settings['search_bar_category'] == '1'){
-        $output .= $this->show_taxonomy('mec_category', 'folder');
-    }
-    
-    if(isset($settings['search_bar_location']) && $settings['search_bar_location'] == '1'){
-        $output .= $this->show_taxonomy('mec_location', 'location-pin');
-    }
-     
-    if(isset($settings['search_bar_organizer']) && $settings['search_bar_organizer'] == '1'){
-        $output .= $this->show_taxonomy('mec_organizer', 'user');
-    }
-    if(isset($settings['search_bar_speaker']) && $settings['search_bar_speaker'] == '1'){
-        $output .= $this->show_taxonomy('mec_speaker', 'microphone');
-    }
-     
-    if(isset($settings['search_bar_tag']) && $settings['search_bar_tag'] == '1'){
-        $output .= $this->show_taxonomy(apply_filters('mec_taxonomy_tag', ''), 'tag');
-    }
-    if(isset($settings['search_bar_label']) && $settings['search_bar_label'] == '1'){
-        $output .= $this->show_taxonomy('mec_label', 'pin');
+    foreach ($taxonomies as $tax) {
+        if (isset($settings[$tax['key']]) && $settings[$tax['key']] == '1') {
+            $output .= $this->show_taxonomy($tax['taxonomy'], $tax['icon']);
+        }
     }
     $output .= '</div>';
 }
+
 $output .= '<input type="text" name="fecha" id="datepicker" autocomplete="off" style="display:none;">
 <button type="button" class="mec-icon-calendario" id="calendario"></button>';
-$output .= '<input class="mec-search-bar-input" id="mec-search-bar-input" type="submit" alt="'.esc_html__('Buscar', 'modern-events-calendar-lite').'" value="'.esc_html__('Buscar', 'modern-events-calendar-lite').'" /><input type="hidden" name="post_type" value="mec-events">';
-$output .= '</form></div>';?>
-<?= MEC_kses::form($output);?>
-<script>
-jQuery(document).ready(function($){
-    jQuery("#keyword").typeWatch(
-    {
-        wait: 400,
-        callback: function(value)
-        {
-            if(!value || value == "")
-            {
-                jQuery('#mec-ajax-search-result-wrap').css({opacity: '0', visibility: 'hidden' });
-            }else{
-                var keyword = jQuery('#keyword').val(),
-                    minLength = 3,
-                    searchWrap = jQuery('.mec-search-bar-wrap');
-                var category = '',
-                    location = '',
-                    organizer = '',
-                    speaker = '',
-                    tag = '',
-                    label = '';
-                if(keyword.length >= minLength) keyword = jQuery('#keyword').val();
-                if(keyword.length == 0) keyword = 'empty';
-                if(jQuery('#category').length > 0)
-                {
-                    if(searchWrap.find('#category').val().length !== 0) category = searchWrap.find('#category').val();
-                }
-
-                if(jQuery('#location').length > 0)
-                {
-                    if(searchWrap.find('#location').val().length !== 0) location = searchWrap.find('#location').val();
-                }
-
-                if(jQuery('#organizer').length > 0)
-                {
-                    if(searchWrap.find('#organizer').val().length !== 0) organizer = searchWrap.find('#organizer').val();
-                }
-
-                if(jQuery('#speaker').length > 0)
-                {
-                    if(searchWrap.find('#speaker').val().length !== 0) speaker = searchWrap.find('#speaker').val();
-                }
-
-                if(jQuery('#tag').length > 0)
-                {
-                    if(searchWrap.find('#tag').val().length !== 0) tag = searchWrap.find('#tag').val();
-                }
-
-                if(jQuery('#label').length > 0)
-                {
-                    if(searchWrap.find('#label').val().length !== 0) label = searchWrap.find('#label').val();
-                }
-
-                jQuery.ajax(
-                {
-                    url: '<?= admin_url('admin-ajax.php'); ?>',
-                    type: 'post',
-                    data: {
-                        action: 'mec_get_ajax_search_data',
-                        keyword: keyword,
-                        length : keyword.length,
-                        category: category,
-                        location: location,
-                        organizer: organizer,
-                        speaker: speaker,
-                        tag: tag,
-                        label: label
-                    },
-                    success: function(data)
-                    {
-                        jQuery('#mec-ajax-search-result-wrap').css(
-                        {
-                            opacity: '1',
-                            visibility: 'visible'
-                        });
-
-                        if(keyword != 'empty') jQuery('.mec-ajax-search-result-events').html(data);
-                    }
-                });
-            }
-        }
-    });
-    jQuery("#keyword").ready(function($)
-    {
-        $(".mec-ajax-search-result").keypress(function(e)
-        {
-            if(e.which == 13) return false;
-        });
-    });
-});
-</script>
-
+$output .= '<input class="mec-search-bar-input" id="mec-search-bar-input" type="submit" alt="' . esc_html__('Buscar', 'modern-events-calendar-lite') . '" value="' . esc_html__('Buscar', 'modern-events-calendar-lite') . '" /><input type="hidden" name="post_type" value="mec-events">';
+$output .= '</form></div>';
+?>
+<?= MEC_kses::form($output); ?>
