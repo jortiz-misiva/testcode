4991b428fcba04c107de3e9ef856827761429163
jortiz-misiva <jrortiz85@hotmail.com>
2024-09-13 16:10:00 -0500

    cambio and &&

--- render.php
+++ render.php
@@ -13,11 +13,12 @@ if (!defined('MECEXEC')) {
 ?>
 <div class="mec-wrap <?= esc_attr($event_colorskin); ?>">
 	<div class="mec-event-list-<?= esc_attr($this->style); ?>">
-		<?php   foreach($this->events as $date=>$events){?>
-            <?php   $month_id = date('Ym', strtotime($date));
-                    if($this->month_divider and $month_id != $current_month_divider){
-                        $current_month_divider = $month_id; ?>
-                    <div class="mec-month-divider" data-toggle-divider="mec-toggle-<?= date('Ym', strtotime($date)); ?>-<?= esc_attr($this->id); ?>"><span><?= esc_html($this->main->date_i18n('F Y', strtotime($date))); ?></span><i class="mec-sl-arrow-down"></i></div>
+		<?php   foreach($this->events as $date=>$events){   ?>
+            <?php
+                $month_id = date('Ym', strtotime($date));
+                if($this->month_divider && $month_id != $current_month_divider){
+                    $current_month_divider = $month_id; ?>
+                <div class="mec-month-divider" data-toggle-divider="mec-toggle-<?= date('Ym', strtotime($date)); ?>-<?= esc_attr($this->id); ?>"><span><?= esc_html($this->main->date_i18n('F Y', strtotime($date))); ?></span><i class="mec-sl-arrow-down"></i></div>
             <?php   }
              foreach($events as $event){
                     $map_events[] = $event;
